afba6a53740be6dc6795849ddc8e0cb08b81432b
shiba inu <jefferson121996@gmail.com>
2023-01-23 17:34:31 -0500

    saving high score in localstorage

--- assets/game.js
+++ assets/game.js
@@ -159,6 +159,23 @@ var endGame = function () {
         window.alert("You've lost your robot in battle.");
     }
     window.alert("The game has now ended. Let's see how we did!");
+
+    // check localStorage for high score, if it's not there, use 0
+    var highScore = localStorage.getItem("highScore");
+    if (highScore === null) {
+        highScore = 0;
+    }
+    // if player has more money than the high score, player has new high score!
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highScore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+        alert(playerInfo.name + " now has a high score of " + playerInfo.money + " !");
+    }
+    else{
+        alert(playerInfo.name + " did not beat the high score of" + highScore + " . Maybe next time!");
+    }
+
+    // ask the player if they would like to play again.
     var playAgainConfirm = window.confirm("Would you like to play again?");
 
     if(playAgainConfirm) {
